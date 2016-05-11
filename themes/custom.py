2697c3cda47ee5741f5046c02544c26fabb87917
DanilF <dflor003@gmail.com>
2016-05-10 23:32:19 -0400

    Added custom settings

---
+++ themes/custom.py
@@ -0,0 +1,36 @@
+
+class Color(DefaultColor):
+    USERNAME_FG = 15
+    USERNAME_BG = 8
+    USERNAME_ROOT_BG = 1
+
+    HOSTNAME_FG = 15
+    HOSTNAME_BG = 8
+
+    HOME_SPECIAL_DISPLAY = True
+    PATH_BG = 61
+    PATH_FG = 15
+    CWD_FG = 15
+    SEPARATOR_FG = 7
+
+    READONLY_BG = 1
+    READONLY_FG = 15
+
+    REPO_CLEAN_BG = 2
+    REPO_CLEAN_FG = 15
+    REPO_DIRTY_BG = 1
+    REPO_DIRTY_FG = 15
+
+    JOBS_FG = 14
+    JOBS_BG = 8
+
+    CMD_PASSED_BG = 8
+    CMD_PASSED_FG = 15
+    CMD_FAILED_BG = 11
+    CMD_FAILED_FG = 0
+
+    SVN_CHANGES_BG = REPO_DIRTY_BG
+    SVN_CHANGES_FG = REPO_DIRTY_FG
+
+    VIRTUAL_ENV_BG = 2
+    VIRTUAL_ENV_FG = 0
